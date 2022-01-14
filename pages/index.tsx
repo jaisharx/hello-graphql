8074d3643aa1d79ce325387654f447086b1e47d5
Jai Sharma <sendtojsharma@gmail.com>
2022-01-14 20:10:45 +0530

    adding spaceX apis and using it to fetch launches

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,45 @@
+import { useEffect, useState } from 'react'
+import { Box, Heading, Text } from '@chakra-ui/react'
+import { request, gql } from 'graphql-request'
 import type { NextPage } from 'next'
 
+const LAUNCHES_QUERY = gql`
+  {
+    launchesPast(limit: 10) {
+      id
+      mission_name
+    }
+  }
+`
+
+const GRAPHQL_ENDPOINT = 'https://api.spacex.land/graphql/'
+
+type Launch = {
+  id: string
+  mission_name: string
+}
+
 const Home: NextPage = () => {
-  return <></>
+  const [launches, setLaunches] = useState<Launch[]>()
+
+  useEffect(() => {
+    async function fetchLaunches() {
+      const response = await request(GRAPHQL_ENDPOINT, LAUNCHES_QUERY)
+
+      setLaunches(response.launchesPast)
+    }
+
+    fetchLaunches()
+  }, [])
+
+  return (
+    <Box maxW="container.md" mx="auto" mt="8">
+      <Heading>Hello, GraphQL!</Heading>
+      {launches?.map((launch) => (
+        <Text key={launch.id}>{launch.mission_name}</Text>
+      ))}
+    </Box>
+  )
 }
 
 export default Home
