75105636c675a8ae22bb55d9066da2e04f677c54
Jai Sharma <sendtojsharma@gmail.com>
2022-01-15 12:56:57 +0530

    adding spinner for loading indicator

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,8 @@ import {
   Heading,
   HStack,
   Spacer,
+  Spinner,
   Text,
-  chakra,
 } from '@chakra-ui/react'
 import { request, gql } from 'graphql-request'
 import { motion } from 'framer-motion'
@@ -25,7 +25,6 @@ const LAUNCHES_QUERY = gql`
 `
 
 const MotionBox = motion<BoxProps>(Box)
-const ChakraBox = chakra(motion.div)
 
 const GRAPHQL_ENDPOINT = 'https://api.spacex.land/graphql/'
 
@@ -34,12 +33,36 @@ type Launch = {
   mission_name: string
 }
 
-const Launches = () => {
+type CompProps = {
+  isVisible: boolean
+  isLoading: boolean
+  setShow: Dispatch<SetStateAction<boolean>>
+}
+
+const ToogleVisibilty = ({ isVisible, isLoading, setShow }: CompProps) => {
+  return (
+    <HStack>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <Button onClick={() => setShow((show) => !show)} size="sm">
+          {isVisible ? 'Show' : 'Hide'}
+        </Button>
+      )}
+    </HStack>
+  )
+}
+
+const Home: NextPage = () => {
   const [launches, setLaunches] = useState<Launch[]>()
+  const [isLoading, setIsLoading] = useState(true)
+  const [show, setShow] = useState(true)
+
   useEffect(() => {
     async function fetchLaunches() {
       const response = await request(GRAPHQL_ENDPOINT, LAUNCHES_QUERY)
 
+      setIsLoading(false)
       setLaunches(response.launchesPast)
     }
 
@@ -47,41 +70,22 @@ const Launches = () => {
   }, [])
 
   return (
-    <Box mt="2">
-      {launches?.map((launch) => (
-        <Text key={launch.id}>{launch.mission_name}</Text>
-      ))}
-    </Box>
-  )
-}
-
-type CompProps = {
-  isVisible: boolean
-  setShow: Dispatch<SetStateAction<boolean>>
-}
-
-const ToogleVisibilty = ({ isVisible, setShow }: CompProps) => {
-  return (
-    <HStack>
-      <Button onClick={() => setShow((show) => !show)} size="sm">
-        {isVisible ? 'Show' : 'Hide'}
-      </Button>
-    </HStack>
-  )
-}
-
-const Home: NextPage = () => {
-  const [show, setShow] = useState(true)
-
-  return (
     <Box maxW="container.md" mx="auto" mt="8">
       <HStack>
         <Heading>Hello, GraphQL!</Heading>
         <Spacer />
-        <ToogleVisibilty isVisible={show} setShow={setShow} />
+        <ToogleVisibilty
+          isVisible={show}
+          isLoading={isLoading}
+          setShow={setShow}
+        />
       </HStack>
-      <MotionBox animate={{ opacity: show ? 1 : 0 }} bg="whatsapp.100">
-        <Launches />
+      <MotionBox animate={{ opacity: show ? 1 : 0 }}>
+        <Box mt="2">
+          {launches?.map((launch) => (
+            <Text key={launch.id}>{launch.mission_name}</Text>
+          ))}
+        </Box>
       </MotionBox>
     </Box>
   )
